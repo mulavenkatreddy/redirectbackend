0856017c644505a8b0e19c1eb8c036443fa5c930
BLVenkat <venkatreddy.mulaa@gmail.com>
2019-08-31 17:31:00 +0530

    url generation part is done

--- src/main/java/com/bridgelabz/interceptors/controller/InterceptorController.java
+++ src/main/java/com/bridgelabz/interceptors/controller/InterceptorController.java
@@ -1,37 +1,67 @@
 package com.bridgelabz.interceptors.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@CrossOrigin(origins="http://localhost:4200",allowedHeaders="*",exposedHeaders={"jwtTokens"})
-public class InterceptorController{
 
-	@GetMapping(value = "/get")
-	public String get(HttpServletRequest request,HttpServletResponse httpResponse) {
+@RestController
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", exposedHeaders = { "jwtTokens" })
+public class InterceptorController {
+
+	static List<String> urllist = new ArrayList<String>();
+	
+	@GetMapping(value = "/get/{id}")
+	public String get(HttpServletRequest request, HttpServletResponse httpResponse, @PathVariable String id) {
 		try {
 			
-			httpResponse.sendRedirect("http://localhost:4200/hello");
-			System.err.println("getheader : "+request.getHeader("referer"));
-			 	System.err.println("getheader : "+request.getHeader("Referer"));
-			System.err.println("request origin: "+request.getHeader("Origin"));
-		} catch (IOException e) {
+				System.out.println(id);
+				
+				if (urllist.contains(id))
+						{
+			httpResponse.sendRedirect("https://reg.bridgelabz.com/registerByCode");
+				}
+				else {
+					httpResponse.sendRedirect("https://www.google.com");
+				}
+
+			System.err.println("getheader : " + request.getHeader("Referer"));
 			
+		} catch (IOException e) {
+
 			e.printStackTrace();
 		}
 		return "hello";
 	}
-	@PostMapping(value = "/post")
-	public String post(@RequestParam String msg ) {
+	
+	@GetMapping(value = "/generateurl")
+	public String  generateUrl() {
 		
+		UUID uuid = UUID.randomUUID();
+		System.out.println("UUID : "+uuid);
+		urllist.add(uuid.toString());
+		return "http://192.168.1.191:8080/get/"+uuid.toString();
+	}
+	
+	//to create the link 
+	//create a link with uuid
+	//where you want to redirect url
+	//hitcount 
+
+	@PostMapping(value = "/post")
+	public String post(@RequestParam String msg) {
+
 		return msg;
 	}
 }
